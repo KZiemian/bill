b526f17d0da6099ecc0dc136c88848f94f92aa34
Kamil Ziemian <kziemianfvt@gmail.com>
2017-05-12 21:44:33 +0200

    Code style

--- examples/snowpeople/main.cpp
+++ examples/snowpeople/main.cpp
@@ -3,18 +3,28 @@
 #include "../../headers/billGLfunctions.h"
 
 
+
+// **********************************************************************
+
+
+
 void drawSnowMan(); 
 void renderScene(void);
 
-int main(int argc, char **argv){
-  bill::Window window(argc,argv);
+int main(int argc, char **argv) {
+  bill::Window window(argc, argv);
   window.set_processNormalKeys(bill::GLaux::processNormalKeys);
   window.set_processNormalKeysUp(bill::GLaux::processNormalKeysUp);
   window.set_renderScene(renderScene);
   window.set_mainLoop(renderScene);
   window.initiate();
+
+
   return 0;
 }
+
+
+// **********************************************************************
 
 void drawSnowMan() {
   
@@ -24,30 +34,31 @@ void drawSnowMan() {
   
   // Draw Body
   glTranslatef(0.0f ,0.75f, 0.0f);
-  glutSolidSphere(0.75f,20,20);
+  glutSolidSphere(0.75f, 20, 20);
   
   // Draw Head
   glTranslatef(0.0f, 1.0f, 0.0f);
-  glutSolidSphere(0.25f,20,20);
+  glutSolidSphere(0.25f, 20, 20);
   
   // Draw Eyes
   glPushMatrix();
-  glColor3f(0.0f,0.0f,0.0f);
+  glColor3f(0.0f, 0.0f, 0.0f);
   glTranslatef(0.05f, 0.10f, 0.18f);
-  glutSolidSphere(0.05f,10,10);
+  glutSolidSphere(0.05f, 10, 10);
   glTranslatef(-0.1f, 0.0f, 0.0f);
-  glutSolidSphere(0.05f,10,10);
+  glutSolidSphere(0.05f, 10, 10);
   glPopMatrix();
   
   // Draw Nose
   glColor3f(1.0f, 0.5f , 0.5f);
-  glRotatef(0.0f,1.0f, 0.0f, 0.0f);
-  glutSolidCone(0.08f,0.5f,10,2);
+  glRotatef(0.0f, 1.0f, 0.0f, 0.0f);
+  glutSolidCone(0.08f, 0.5f, 10, 2);
 }
 
 void renderScene(void) {
   
-  if(bill::GLaux::moveParallel|bill::GLaux::movePerpendicular|bill::GLaux::rotateParallel|bill::GLaux::rotatePerpendicular)
+  if(bill::GLaux::moveParallel | bill::GLaux::movePerpendicular
+     | bill::GLaux::rotateParallel | bill::GLaux::rotatePerpendicular)
     bill::GLaux::computePos();
  
   // From: http://www.lighthouse3d.com/tutorials/glut-tutorial/
@@ -61,7 +72,7 @@ void renderScene(void) {
   glLoadIdentity();
   
   // Set the correct perspective.
-  gluPerspective(45.0,800.0/600.0,0.1,1000000000.0);
+  gluPerspective(45.0, (800.0 / 600.0), 0.1, 1000000000.0);
   
   // Get Back to the Modelview
   glMatrixMode(GL_MODELVIEW);
@@ -82,10 +93,10 @@ void renderScene(void) {
   
   // Draw 36 SnowMen
   
-  for(int i = -3; i < 3; i++)
-    for(int j=-3; j < 3; j++) {
+  for (int i = -3; i < 3; i++)
+    for (int j=-3; j < 3; j++) {
       glPushMatrix();
-      glTranslatef(i*10.0,0,j * 10.0);
+      glTranslatef(i*10.0, 0, j * 10.0);
       drawSnowMan();
       glPopMatrix();
     }
