45f8d673db3629e976a6d8059585b42b3e6e05a2
Kamil Ziemian <kziemianfvt@gmail.com>
2017-05-12 21:35:32 +0200

    Code style

--- examples/planet/main.cpp
+++ examples/planet/main.cpp
@@ -8,6 +8,10 @@
 #include "../../headers/billintegrators.h"
 #include "planet.h"
 
+
+
+// **********************************************************************
+
 void renderScene(void);
 void mainLoop(void);
 
@@ -15,38 +19,55 @@ bill::BillSetOfPoints SetOfPoints;
 
 bill::BillEngine engine;
 
-int main(int argc, char **argv){
+// **********************************************************************
 
-  bill::GLaux::eye=bill::vector({-10,0,0});
-  bill::GLaux::center=bill::vector({0,0,0});
 
-  SetOfPoints.AddPoint(std::shared_ptr<planet>(new planet(bill::ModEuler,1.,bill::vector({0.0,1.0,0.0}),bill::vector({0.0,0.0,1.0}),0.05)));
+
+
+
+int main(int argc, char **argv) {
+
+  bill::GLaux::eye = bill::vector({-10, 0, 0});
+  bill::GLaux::center = bill::vector({0, 0, 0});
+
+  SetOfPoints.AddPoint(std::shared_ptr<planet>(new planet(bill::ModEuler, 1.0,
+							  bill::vector({0.0,
+								1.0, 0.0}),
+							  bill::vector({0.0,
+								0.0, 1.0}),
+							  0.05)));
   engine = bill::BillEngine(SetOfPoints);
 
-  bill::Window window(argc,argv);
+  bill::Window window(argc, argv);
   window.set_processNormalKeys(bill::GLaux::processNormalKeys);
   window.set_processNormalKeysUp(bill::GLaux::processNormalKeysUp);
   window.set_renderScene(renderScene);
   window.set_mainLoop(mainLoop);
 
   window.initiate();
+
+
   return 0;
 }
 
-void mainLoop(void){
+
+// **********************************************************************
+
+void mainLoop(void) {
   engine.step();
   renderScene();
 }
 
 
 void renderScene(void) {
-  if(bill::GLaux::moveParallel|bill::GLaux::movePerpendicular|bill::GLaux::rotateParallel|bill::GLaux::rotatePerpendicular)
+  if(bill::GLaux::moveParallel | bill::GLaux::movePerpendicular
+     | bill::GLaux::rotateParallel | bill::GLaux::rotatePerpendicular)
     bill::GLaux::computePos();
  
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(45.0,800.0/600.0,0.1,1000000000.0);
+  gluPerspective(45.0, (800.0 / 600.0), 0.1, 1000000000.0);
   glMatrixMode(GL_MODELVIEW);
   
   
--- examples/planet/planet.cpp
+++ examples/planet/planet.cpp
@@ -1,12 +1,21 @@
 #include "planet.h"
 
-bill::vector planet::Force(){
+
+
+// **********************************************************************
+
+
+
+bill::vector planet::Force() {
   bill::vector Planet = std::get<0>(present);
 
-  return M*mass*((Sun - Planet)/pow(bill::vector::norm(Sun - Planet),3));
+  return M*mass*((Sun - Planet) / pow(bill::vector::norm(Sun - Planet), 3));
 }
 
-planet::planet(bill::BillIntegrator algorithm, double M, bill::vector position, bill::vector velocity, bill::vector Sun, double mass, bill::vector color):bill::BillMaterialPoint(algorithm,position,velocity,mass,color){
-	this->M = M;
-	this->Sun = Sun;
+planet::planet(bill::BillIntegrator algorithm, double M,
+	       bill::vector position, bill::vector velocity,
+	       bill::vector Sun, double mass, bill::vector color):
+  bill::BillMaterialPoint(algorithm, position, velocity, mass, color) {
+  this->M = M;
+  this->Sun = Sun;
 }
