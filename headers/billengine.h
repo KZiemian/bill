f6fe8e66b9864cdd3321f9e9c56fdb8297eba9e2
Kamil Ziemian <kziemianfvt@gmail.com>
2017-05-12 20:37:06 +0200

    Code style

--- headers/billengine.h
+++ headers/billengine.h
@@ -5,21 +5,28 @@
 #include <cmath>
 #include "billmaterialpoint.h"
 
-namespace bill{
 
-class BillEngine{
-private:
-  BillSetOfPoints* set;
-  unsigned int starttime;
-  unsigned int now;
-  double timestep;
 
-public:
+/* ********************************************************************** */
 
-  BillEngine();
-  BillEngine(BillSetOfPoints& set);
-  void step();
-}; //end of class BillEngine
+
+
+namespace bill {
+
+  class BillEngine {
+  public:
+    BillEngine();
+    BillEngine(BillSetOfPoints& set);
+    void step();
+
+
+
+  private:
+    BillSetOfPoints* set;
+    unsigned int starttime;
+    unsigned int now;
+    double timestep;
+  }; //end of class BillEngine
 
 } //end of namespace bill
 #endif
