9ac8618d797aea5ca7715031b227f401af9dd56b
Kamil Ziemian <kziemianfvt@gmail.com>
2017-05-13 14:24:25 +0200

    Code style

--- exercises/ex4/oscillator.h
+++ exercises/ex4/oscillator.h
@@ -6,8 +6,37 @@
 #include "../../headers/billmaterialpoint.h"
 #include "../../headers/billvector.h"
 
-class oscillator : public bill::BillMaterialPoint{
-protected:
+
+
+/* ********************************************************************** */
+
+
+
+class oscillator : public bill::BillMaterialPoint {
+public:
+  oscillator(bill::BillIntegrator algorithm, double k, double l,
+	     bill::vector position = bill::vector({0., 0., 0.}),
+	     bill::vector velocity = bill::vector({0. ,0. ,0.}),
+	     double mass = 1.0,
+	     bill::vector color = bill::vector({1.0, 0.0, 0.0}),
+	     double step = 0.2);
+  virtual ~oscillator(){};
+  void set_right(std::shared_ptr<oscillator> r); // ustawianie wsk. do prawego
+  void set_left(std::shared_ptr<oscillator> l); // ustawianie wsk. do lewego
+  void set_2nd_right(std::shared_ptr<oscillator> r);
+  // ustawianie wsk. do prawego
+  void set_2nd_left(std::shared_ptr<oscillator> l);
+  // ustawianie wsk. do lewego
+  void set_3rd_right(std::shared_ptr<oscillator> r);
+  // ustawianie wsk. do prawego
+  void set_3rd_left(std::shared_ptr<oscillator> l);
+  // ustawianie wsk. do lewego
+
+  virtual bill::vector Force();
+
+
+
+ protected:
   double k; // współczynnik sprężystości
   double kp;// współczynnik sprężystości 2go sąsiada
   double kb;// współczynnik sprężystości 3go sąsiada
@@ -20,17 +49,5 @@ protected:
   std::shared_ptr<oscillator> second_right; // wsk. do 2go prawego
   std::shared_ptr<oscillator> third_left; // wsk. do 3go lewego
   std::shared_ptr<oscillator> third_right; // wsk. do 3go prawego
-
-public:
-  oscillator(bill::BillIntegrator algorithm, double k, double l, bill::vector position=bill::vector({0.,0.,0.}), bill::vector velocity=bill::vector({0.,0.,0.}), double mass=1.0, bill::vector color=bill::vector({1.0,0.0,0.0}), double step=0.2);
-  virtual ~oscillator(){};
-  void set_right(std::shared_ptr<oscillator> r); // ustawianie wsk. do prawego
-  void set_left(std::shared_ptr<oscillator> l); // ustawianie wsk. do lewego
-  void set_2nd_right(std::shared_ptr<oscillator> r); // ustawianie wsk. do prawego
-  void set_2nd_left(std::shared_ptr<oscillator> l); // ustawianie wsk. do lewego
-  void set_3rd_right(std::shared_ptr<oscillator> r); // ustawianie wsk. do prawego
-  void set_3rd_left(std::shared_ptr<oscillator> l); // ustawianie wsk. do lewego
-
-  virtual bill::vector Force();
 }; //end of class oscillator
 #endif
